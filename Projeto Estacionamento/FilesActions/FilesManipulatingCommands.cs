b69e7ff1b72b35df2dae0708f6744bd8f79745a9
Anderson Vargas <andersomrobertomachadodevargas@gmail.com>
2025-08-15 16:41:46 -0300

    Move some events logic code to a specif class

--- Projeto Estacionamento/FilesActions/FilesManipulatingCommands.cs
+++ Projeto Estacionamento/FilesActions/FilesManipulatingCommands.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Projeto_Estacionamento.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,27 @@ namespace Projeto_Estacionamento.FilesActions
 {
     public static class FilesManipulatingCommands
     {
+        public static void SaveJasonFile()
+        {
+            try
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Arquivos JSON (*.json)|*.json|Todos os arquivos (*.*)|*.*";
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string jsonString = System.Text.Json.JsonSerializer.Serialize(Infocar.infocarsList);
+                        File.WriteAllText(saveFileDialog.FileName, jsonString);
+
+                        MessageBox.Show("Arquivo salvo com sucesso", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Erro inesperado", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
